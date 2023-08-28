ccac982f5226aec3bd039ae9f3f01ecbbb92de2c
zumrudisgandar <zzumus@mail.ru>
2023-08-28 17:13:01 +0400

    Changes in color, footer, and mongoDB

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://admin-zumrud:Test123@cluster0.f14wlq0.mongodb.net/todolistDB", { useNewUrlParser: true })
 
 const ItemsSchema = new mongoose.Schema({
     name: String
